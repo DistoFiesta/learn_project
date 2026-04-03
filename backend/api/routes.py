90bceae1aa0e6720019c9a0b12983ffc514cc82e
Pushkarev <starlightmember563@gmail.com>
2026-04-03 13:36:52 +0700

    Работа была окончена. Сайт был спокойно запущен

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -4,10 +4,10 @@ from backend.schemas.item_schema import ItemsOutSchema
 
 def register_routes(app):
     @app.get('/api/status')
-    @app.output(StatusOutSchema())
+    @app.output(StatusOutSchema)
     def api_status():
         return get_status_data()
     @app.get('/api/items')
-    @app.output(ItemsOutSchema())
+    @app.output(ItemsOutSchema)
     def api_items():
         return {"items": get_items_data()}
--- backend/services/status_service.py
+++ backend/services/status_service.py
@@ -6,8 +6,8 @@ def get_status_data():
         "items_count": 3
     }
 def get_items_data():
-    return {
+    return [
         {id: 1, "name": "Товар 1"},
         {id: 2, "name": "Товар 2"},
         {id: 3, "name": "Товар 3"}
-    }
+    ]
